ef61fcc6012eea4c38acace7909bb4ff2ffb4ea6
Stephen Lake <stephen.j.lake@gmail.com>
2015-04-17 09:02:55 +0100

    Reinstate deflation process

--- src/main/java/com/ringingmaster/extraction/MethodLibraryBuilder.java
+++ src/main/java/com/ringingmaster/extraction/MethodLibraryBuilder.java
@@ -41,6 +41,7 @@ public class MethodLibraryBuilder {
 
 		final SerializableNotationList notations = methodExtractor.extractNotations();
 		writeNotations(notations);
+		writeNotationsDeflated(notations);
 //TODO	    final SerializableNotationList notationsRead = readNotations();
 //TODO	    final boolean match = notations.equals(notationsRead);
 //TODO		if (match) {
@@ -54,15 +55,12 @@ public class MethodLibraryBuilder {
 	}
 
 	private static void writeNotations(final SerializableNotationList notations) {
-		log.info("serialising to [" + ALL_NOTATIONS_DEFLATED_OUTPUT + "]");
+		log.info("serialising to [" + ALL_NOTATIONS_OUTPUT + "]");
 
 		FileOutputStream fos = null;
 		DeflaterOutputStream dos = null;
 		try {
-//			dos = new DeflaterOutputStream(fos);
 			fos = new FileOutputStream(ALL_NOTATIONS_OUTPUT_FOLDER + ALL_NOTATIONS_OUTPUT);
-
-
 
 			final JAXBContext jc = JAXBContext.newInstance( "com.concurrentperformance.ringingmaster.generated.notation.persist" );
 			final Marshaller m = jc.createMarshaller();
@@ -70,6 +68,45 @@ public class MethodLibraryBuilder {
 					Boolean.TRUE );
 
 			m.marshal(new ObjectFactory().createSerializableNotationList(notations), fos);
+
+			// Also to Std out for debug.
+			//m.marshal(new ObjectFactory().createSerializableNotationList(notations), System.out);
+
+		} catch (final IOException e) {
+			log.error("Exception serializing notations", e);
+		} catch (final JAXBException e) {
+			log.error("Exception marshalling notations", e);
+		}
+		finally {
+			try {
+				if (fos != null) {
+					fos.close();
+				}
+			} catch (final IOException e) {}
+			try {
+				if (fos != null) {
+					fos.close();
+				}
+			} catch (final IOException e) {}
+		}
+	}
+
+	private static void writeNotationsDeflated(final SerializableNotationList notations) {
+		log.info("serialising to [" + ALL_NOTATIONS_DEFLATED_OUTPUT + "]");
+
+		FileOutputStream fos = null;
+		DeflaterOutputStream dos = null;
+		try {
+
+			fos = new FileOutputStream(ALL_NOTATIONS_OUTPUT_FOLDER + ALL_NOTATIONS_DEFLATED_OUTPUT);
+			dos = new DeflaterOutputStream(fos);
+
+			final JAXBContext jc = JAXBContext.newInstance( "com.concurrentperformance.ringingmaster.generated.notation.persist" );
+			final Marshaller m = jc.createMarshaller();
+			m.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT,
+					Boolean.TRUE );
+
+			m.marshal(new ObjectFactory().createSerializableNotationList(notations), dos);
 
 			// Also to Std out for debug.
 			//m.marshal(new ObjectFactory().createSerializableNotationList(notations), System.out);
